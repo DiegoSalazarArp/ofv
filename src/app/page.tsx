ac8d2f63edca227b5e48dda0b701382fb97f61bd
Diego Salazar <diego.salazar@grupomok.com>
2024-11-21 10:53:21 -0300

    fix dashboard and add charts to the main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
-
+import { redirect } from "next/navigation";
 export default function Home() {
+
+  redirect("/login");
 
   return (
     <div className="h-screen flex items-center justify-center p-2 font-[family-name:var(--font-geist-sans)]">
